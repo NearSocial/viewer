a6da6b0bd3ee79fc25035b8e3b5cd42f93237576
Evgeny Kuzyakov <ek@nearprotocol.com>
2022-11-29 10:19:51 -0800

    Sidebar css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -23,16 +23,18 @@ export function Sidebar(props) {
 
   return (
     <div className="min-vh-100 vh-100">
-      <div id="sidebar" className={show ? "show" : ""}>
-        <div className="h-100 d-flex flex-column bg-light">
+      <div
+        id="sidebar"
+        className={`sidebar-items d-flex flex-column bg-light ${
+          show ? "show" : ""
+        } align-items-center`}
+      >
+        <div>
           <OverlayTrigger
             placement="right"
             overlay={<Tooltip>Near Social</Tooltip>}
           >
-            <Link
-              className="d-block py-3 text-center link-dark text-decoration-none border-bottom"
-              to="/"
-            >
+            <Link className="d-block link-dark" to="/">
               <img
                 src={Logo}
                 alt="Near Social logo horizontal"
@@ -41,137 +43,129 @@ export function Sidebar(props) {
               />
             </Link>
           </OverlayTrigger>
-
-          <div className="flex-grow-1 flex-shrink-1 overflow-hidden">
-            {NearConfig.widgets.navigationApps && (
-              <Widget src={NearConfig.widgets.navigationApps} />
-            )}
-          </div>
+        </div>
+        <div className="apps flex-grow-1 flex-shrink-1 overflow-hidden flex-column sidebar-items">
+          {NearConfig.widgets.navigationApps && (
+            <Widget src={NearConfig.widgets.navigationApps} />
+          )}
+        </div>
+        <div>
           <OverlayTrigger
             placement="right"
             overlay={<Tooltip>Notifications</Tooltip>}
           >
-            <div className="d-block py-1 text-center link-dark text-decoration-none border-top">
+            <div>
               <Widget src={NearConfig.widgets.notificationButton} />
             </div>
           </OverlayTrigger>
-          <div className="dropend border-top">
-            <OverlayTrigger
-              placement="right"
-              overlay={<Tooltip>Widget</Tooltip>}
+        </div>
+        <div className="dropend">
+          <OverlayTrigger placement="right" overlay={<Tooltip>Widget</Tooltip>}>
+            <a
+              href="#"
+              className="d-flex align-items-center justify-content-center link-dark text-decoration-none dropdown-toggle"
+              id="dropdownWidgets"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
             >
-              <a
-                href="#"
-                className="d-flex align-items-center justify-content-center p-2 link-dark text-decoration-none dropdown-toggle"
-                id="dropdownWidgets"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <i className="bi bi-gear-fill fs-4"></i>
-              </a>
-            </OverlayTrigger>
-            <ul
-              className="dropdown-menu shadow"
-              aria-labelledby="dropdownWidgets"
-            >
+              <i className="bi bi-gear-fill fs-4"></i>
+            </a>
+          </OverlayTrigger>
+          <ul
+            className="dropdown-menu shadow"
+            aria-labelledby="dropdownWidgets"
+          >
+            <li>
+              <Link className="dropdown-item" to="/edit/new">
+                New widget
+              </Link>
+            </li>
+            {widgetSrc?.edit && (
               <li>
-                <Link className="dropdown-item" to="/edit/new">
-                  New widget
+                <Link className="dropdown-item" to={`/edit/${widgetSrc.edit}`}>
+                  {widgetSrc.edit.startsWith(`${props.signedAccountId}/widget/`)
+                    ? "Edit widget"
+                    : "Fork widget"}
                 </Link>
               </li>
-              {widgetSrc?.edit && (
-                <li>
-                  <Link
-                    className="dropdown-item"
-                    to={`/edit/${widgetSrc.edit}`}
-                  >
-                    {widgetSrc.edit.startsWith(
-                      `${props.signedAccountId}/widget/`
-                    )
-                      ? "Edit widget"
-                      : "Fork widget"}
-                  </Link>
-                </li>
-              )}
-              {widgetSrc?.view && (
-                <li>
-                  <Link
-                    className="dropdown-item"
-                    to={`/${NearConfig.widgets.viewSource}?src=${widgetSrc?.view}`}
-                  >
-                    View source
-                  </Link>
-                </li>
-              )}
-            </ul>
-          </div>
-          <div className="dropend border-top">
-            <OverlayTrigger
-              placement="right"
-              overlay={<Tooltip>Account</Tooltip>}
-            >
-              <a
-                href="#"
-                className="d-flex align-items-center justify-content-center px-2 link-dark text-decoration-none dropdown-toggle"
-                id="dropdownUser"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                style={{ paddingTop: "0.666rem", paddingBottom: "0.666rem" }}
-              >
-                <div
-                  className="d-inline-block"
-                  style={{ width: "2em", height: "2em" }}
+            )}
+            {widgetSrc?.view && (
+              <li>
+                <Link
+                  className="dropdown-item"
+                  to={`/${NearConfig.widgets.viewSource}?src=${widgetSrc?.view}`}
                 >
-                  <Widget
-                    src={NearConfig.widgets.profileImage}
-                    props={{
-                      accountId,
-                      className: "d-inline-block",
-                      style: { width: "2em", height: "2em" },
-                    }}
-                  />
-                </div>
-              </a>
-            </OverlayTrigger>
-            <ul className="dropdown-menu shadow" aria-labelledby="dropdownUser">
-              {props.signedIn ? (
-                <>
-                  <li>
-                    <button
-                      className="dropdown-item"
-                      onClick={(e) => withdrawStorage(e)}
-                      title={`Withdraw all available storage`}
-                    >
-                      Withdraw{" "}
-                      {props.availableStorage &&
-                        props.availableStorage.div(1000).toFixed(2)}
-                      kb
-                    </button>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <button
-                      className="dropdown-item"
-                      onClick={(e) => props.logOut(e)}
-                    >
-                      Sign out {props.signedAccountId}
-                    </button>
-                  </li>
-                </>
-              ) : (
+                  View source
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
+        <div className="dropend">
+          <OverlayTrigger
+            placement="right"
+            overlay={<Tooltip>Account</Tooltip>}
+          >
+            <a
+              href="#"
+              className="d-flex align-items-center justify-content-center link-dark text-decoration-none dropdown-toggle"
+              id="dropdownUser"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <div
+                className="d-inline-block"
+                style={{ width: "2em", height: "2em" }}
+              >
+                <Widget
+                  src={NearConfig.widgets.profileImage}
+                  props={{
+                    accountId,
+                    className: "d-inline-block",
+                    style: { width: "2em", height: "2em" },
+                  }}
+                />
+              </div>
+            </a>
+          </OverlayTrigger>
+          <ul className="dropdown-menu shadow" aria-labelledby="dropdownUser">
+            {props.signedIn ? (
+              <>
                 <li>
                   <button
                     className="dropdown-item"
-                    onClick={(e) => props.requestSignIn(e)}
+                    onClick={(e) => withdrawStorage(e)}
+                    title={`Withdraw all available storage`}
                   >
-                    Sign in with NEAR Wallet
+                    Withdraw{" "}
+                    {props.availableStorage &&
+                      props.availableStorage.div(1000).toFixed(2)}
+                    kb
                   </button>
                 </li>
-              )}
-            </ul>
-          </div>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <button
+                    className="dropdown-item"
+                    onClick={(e) => props.logOut(e)}
+                  >
+                    Sign out {props.signedAccountId}
+                  </button>
+                </li>
+              </>
+            ) : (
+              <li>
+                <button
+                  className="dropdown-item"
+                  onClick={(e) => props.requestSignIn(e)}
+                >
+                  Sign in with NEAR Wallet
+                </button>
+              </li>
+            )}
+          </ul>
         </div>
       </div>
       <div className="navbar fixed-top d-sm-none bg-light w-100 pb-2">
