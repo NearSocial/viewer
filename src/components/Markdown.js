73504e7e2e73a048ff900eb3fde47f823350aff6
Evgeny Kuzyakov <ek@proximity.dev>
2023-01-20 15:36:30 -0800

    Add onMention to Markdown tag that parses Near's account IDs (#106)
    - Introduce mentions to Markdown component.
    By default it turns `@mob.near` into bold, but if `onMention` is passed,
    it calls it with the accountId instead.
    
    Example code:
    ```jsx
    const renderMention =
      ((accountId) => (
        <Widget
          src="mob.near/widget/ProfileLine"
          props={{ accountId, hideAccountId: true, tooltip: true }}
        />
      ));
    
    return <Markdown text="Hi @root.near" onMention={renderMention} />;
    ```

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -3,14 +3,20 @@ import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 import React from "react";
-
+import mentions from "./remark/mentions";
 export const Markdown = (props) => {
-  const onLinkClick = props.onLinkClick;
+  const { onLinkClick, text, onMention, ...rest } = props;
   return (
     <ReactMarkdown
-      {...props}
-      plugins={[gfm]}
+      {...rest}
+      plugins={[gfm, mentions]}
       components={{
+        strong({ node, children, ...props }) {
+          if (onMention && node.properties?.accountId) {
+            return onMention(node.properties?.accountId);
+          }
+          return <strong {...props}>{children}</strong>;
+        },
         a: ({ node, ...props }) =>
           onLinkClick ? (
             <a onClick={onLinkClick} {...props} />
@@ -42,7 +48,7 @@ export const Markdown = (props) => {
         },
       }}
     >
-      {props.text}
+      {text}
     </ReactMarkdown>
   );
 };
---
+++ src/components/remark/mentions.js
@@ -0,0 +1,35 @@
+import { findAndReplace } from "mdast-util-find-and-replace";
+
+const mentionRegex =
+  /@((?:(?:[a-z\d]+[-_])*[a-z\d]+.)*(?:[a-z\d]+[-_])*[a-z\d]+)/gi;
+
+export default function mentions() {
+  function replace(value, username, match) {
+    if (
+      /[\w`]/.test(match.input.charAt(match.index - 1)) ||
+      /[/\w`]/.test(match.input.charAt(match.index + value.length)) ||
+      username.length < 3 ||
+      username.length > 64
+    ) {
+      return false;
+    }
+
+    let node = { type: "text", value };
+    node = {
+      type: "strong",
+      children: [node],
+      data: {
+        hProperties: { accountId: username },
+      },
+    };
+
+    return node;
+  }
+
+  function transform(markdownAST) {
+    findAndReplace(markdownAST, mentionRegex, replace);
+    return markdownAST;
+  }
+
+  return transform;
+}
