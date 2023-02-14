99d4364d9fbdbb7e76b9cb7c823113f7a162afe9
Evgeny Kuzyakov <ek@nearprotocol.com>
2023-02-14 14:45:40 -0800

    Change default widget

--- src/data/near.js
+++ src/data/near.js
@@ -61,7 +61,7 @@ export const MainNearConfig = {
   wrapNearAccountId: "wrap.near",
   widgets: {
     image: "mob.near/widget/Image",
-    default: "mob.near/widget/Homepage",
+    default: "ethden.near/widget/BosHomepage",
     viewSource: "mob.near/widget/WidgetSource",
     widgetMetadataEditor: "mob.near/widget/WidgetMetadataEditor",
     widgetMetadata: "mob.near/widget/WidgetMetadata",
