30be407769ea0adf2e6712fbdc0a3518de437226
Patrick Tajima <patrick.tajima@gmail.com>
2023-02-22 16:53:58 -0500

    fix: Update default home page

--- src/data/widgets.js
+++ src/data/widgets.js
@@ -19,7 +19,7 @@ const TestnetWidgets = {
 
 const MainnetWidgets = {
   image: "mob.near/widget/Image",
-  default: "mob.near/widget/Homepage",
+  default: "calebjacob.near/widget/ActivityPage",
   viewSource: "mob.near/widget/WidgetSource",
   widgetMetadataEditor: "mob.near/widget/WidgetMetadataEditor",
   widgetMetadata: "mob.near/widget/WidgetMetadata",
