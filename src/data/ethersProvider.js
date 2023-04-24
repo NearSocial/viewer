3fb241b6a6671380437df27ccf62af0c51126309
Evgeny Kuzyakov <ek@nearprotocol.com>
2023-04-24 10:07:10 -0700

    Add BNB and Arb chains

--- src/data/ethersProvider.js
+++ src/data/ethersProvider.js
@@ -67,8 +67,20 @@ export const onboard = init({
       id: 324,
       token: "ETH",
       label: "zkSync",
-      rpcUrl: "https://zksync2-mainnet.zksync.io"
-    }
+      rpcUrl: "https://zksync2-mainnet.zksync.io",
+    },
+    {
+      id: 56,
+      token: "BNB",
+      label: "Binance Smart Chain Mainnet",
+      rpcUrl: "https://bsc.publicnode.com",
+    },
+    {
+      id: 42161,
+      token: "ETH",
+      label: "Arbitrum One Mainnet",
+      rpcUrl: "https://endpoints.omniatech.io/v1/arbitrum/one/public",
+    },
   ],
   appMetadata: {
     name: "BOS",
