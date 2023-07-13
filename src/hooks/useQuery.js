277f29b2f6dd4e51cf2ad0367c0633fea35396ef
Peter Salomonsen <petersalomonsen@runbox.com>
2023-07-13 11:11:37 +0000

    handle both url search params from before and after hash

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -2,6 +2,13 @@ import { useLocation } from "react-router-dom";
 import React from "react";
 
 export function useQuery() {
-  const search = location.search;
+  let { search } = useLocation();
+  if (location.search) {
+    if (!search) {
+      search = location.search;
+    } else {
+      search += `&${location.search.substring(1)}`;
+    }
+  }
   return React.useMemo(() => new URLSearchParams(search), [search]);
 }
