e70b9b1115671a0f41e4dea87ac1b2fae8a76724
Evgeny Kuzyakov <ek@nearprotocol.com>
2023-07-27 12:35:34 -0700

    Fix prod webpack to route static content correctly

--- webpack.config.js
+++ webpack.config.js
@@ -82,6 +82,7 @@ module.exports = function (env) {
           template: `${paths.publicPath}/index.html`,
           favicon: `${paths.publicPath}/favicon.png`,
           robots: `${paths.publicPath}/robots.txt`,
+          publicPath: "/",
         }),
         new webpack.ProgressPlugin(),
         new webpack.ProvidePlugin({
