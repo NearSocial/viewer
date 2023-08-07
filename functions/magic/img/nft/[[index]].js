1bd1b804fb0db5fd25347b8b0a21a6fb440aa922
Evgeny Kuzyakov <ek@nearprotocol.com>
2023-08-07 14:47:18 -0700

    Add NFT image support

---
+++ functions/magic/img/nft/[[index]].js
@@ -0,0 +1,26 @@
+import { internalImageToUrl } from "../../../common";
+
+export async function onRequest({ request, next, env }) {
+  const url = new URL(request.url);
+  const parts = url.pathname.split("/");
+  if (parts.length !== 6) {
+    return next();
+  }
+  const contractId = parts[4];
+  const tokenId = parts[5];
+
+  const destinationURL = await internalImageToUrl(env, {
+    nft: {
+      contractId,
+      tokenId,
+    },
+  });
+
+  return destinationURL
+    ? new Response(destinationURL, {
+        headers: {
+          "content-type": "text/plain;charset=UTF-8",
+        },
+      })
+    : next();
+}
