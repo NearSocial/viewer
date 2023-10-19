892d2a89ef12765e20bb278f571c9478fb66162a
James Waugh <jwaugh19@gmail.com>
2023-10-19 09:57:16 -0400

    customizable homepage

--- src/data/widgets.js
+++ src/data/widgets.js
@@ -21,7 +21,7 @@ const TestnetWidgets = {
 
 const MainnetWidgets = {
   image: "mob.near/widget/Image",
-  default: "build.sputnik-dao.near/widget/community",
+  default: "build.sputnik-dao.near/widget/dao.page",
   viewSource: "mob.near/widget/WidgetSource",
   widgetMetadataEditor: "mob.near/widget/WidgetMetadataEditor",
   widgetMetadata: "mob.near/widget/WidgetMetadata",
