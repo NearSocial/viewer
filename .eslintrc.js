1e70d7121224bd1e3e9daf837e1364a915212be0
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-11-07 12:35:18 +0000

    feat: init commit design

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    browser: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": 0,
+  },
+};
