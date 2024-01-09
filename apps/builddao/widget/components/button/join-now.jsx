9a4a2d7c44825f32e444beb3e0a781ec6cd61766
root <root@LAPTOP-6CGNQCLE>
2024-01-09 19:22:58 -0300

    FEATURE: Adding joined bullet text if cannot join

--- apps/builddao/widget/components/button/join-now.jsx
+++ apps/builddao/widget/components/button/join-now.jsx
@@ -35,6 +35,22 @@ if (proposal === null) {
 const canJoin =
   accountId && accountId !== proposal.proposer && !isCommunityOrCouncilMember;
 
+const Bullet = styled.div`
+  width: fit-content;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 4px 12px;
+  background: rgba(81, 255, 234, 0.20);
+  color: rgba(81, 255, 234, 1);
+  border: 1px solid rgba(81, 255, 234, 0.20);
+  font-family: Satoshi, sans-serif;
+  font-size: 0.875rem;
+  font-weight: 500;
+  border-radius: 8px;
+`;
+
+
 const Button = styled.a`
   width: max-content;
   all: unset;
@@ -84,6 +100,6 @@ return (
   <Container>
     {canJoin ? (
       <Button href={"/join"}>Join Now</Button>
-    ) : props.children}
+    ) : <Bullet>Joined</Bullet>}
   </Container>
 );
