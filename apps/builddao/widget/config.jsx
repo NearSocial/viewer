b1e8a318954037edc88cfcadb67b5931667f802c
Zeeshan Ahmad <itexpert120@outlook.com>
2024-01-23 20:23:03 +0500

    add css variables to config

--- apps/builddao/widget/config.jsx
+++ apps/builddao/widget/config.jsx
@@ -1,32 +1,63 @@
+const theme = `
+  --stroke-color: rgba(255, 255, 255, 0.2);
+  --bg-1: #0b0c14;
+  --bg-1-hover: #17181c;
+  --bg-1-hover-transparent: rgba(23, 24, 28, 0);
+  --bg-2: ##23242b;
+  --label-color: #fff;
+  --font-color: #fff;
+  --font-muted-color: #cdd0d5;
+  --black: #000;
+  --system-red: #fd2a5c;
+  --yellow: #ffaf51;
+
+  --compose-bg: #23242b;
+
+  --post-bg: #0b0c14;
+  --post-bg-hover: #17181c;
+  --post-bg-transparent: rgba(23, 24, 28, 0);
+
+  --button-primary-bg: #ffaf51;
+  --button-outline-bg: transparent;
+  --button-default-bg: #23242b;
+
+  --button-primary-color: #000;
+  --button-outline-color: #fff;
+  --button-default-color: #cdd0d5;
+
+  --button-primary-hover-bg: #e49b48;
+  --button-outline-hover-bg: rgba(255, 255, 255, 0.2);
+  --button-default-hover-bg: #17181c;
+`;
+
 return {
-  "type": "app",
-  "routes": {
-    "home": {
-      "path": "buildhub.near/widget/page.home",
-      "blockHeight": "final",
-      "init": {
-        "name": "Home",
-        "icon": "bi bi-house"
-      }
+  type: "app",
+  routes: {
+    home: {
+      path: "buildhub.near/widget/page.home",
+      blockHeight: "final",
+      init: {
+        name: "Home",
+        icon: "bi bi-house",
+      },
     },
-    "feed": {
-      "path": "buildhub.near/widget/page.feed",
-      "blockHeight": "final",
-      "init": {
-        "icon": "bi bi-globe"
-      }
+    feed: {
+      path: "buildhub.near/widget/page.feed",
+      blockHeight: "final",
+      init: {
+        icon: "bi bi-globe",
+      },
     },
-    "inspect": {
-      "path": "mob.near/widget/WidgetSource",
-      "blockHeight": "final",
-      "hide": true
+    inspect: {
+      path: "mob.near/widget/WidgetSource",
+      blockHeight: "final",
+      hide: true,
     },
-    "notifications": {
-      "path": "mob.near/widget/NotificationFeed",
-      "blockHeight": "final",
-      "hide": true
-    }
+    notifications: {
+      path: "mob.near/widget/NotificationFeed",
+      blockHeight: "final",
+      hide: true,
+    },
   },
-  "theme": "background-color: white;"
-}
-;
+  theme: theme,
+};
