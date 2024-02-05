a40494485ab2317b1ad1708f9ea53497109e24e2
M-RB3 <rabea9220@gmail.com>
2024-02-05 19:08:19 +0400

    fix login modal

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ function App(props) {
             setupNearFi(),
             setupMathWallet(),
             setupWelldoneWallet(),
-            setupNearSnap(),
+            // setupNearSnap(),
             setupLedger(),
             setupCoin98Wallet(),
             setupRamperWallet(),
