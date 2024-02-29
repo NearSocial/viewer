b768f88903db541125a26fb66e683cf66534ee44
Zeeshan Ahmad <itexpert120@outlook.com>
2024-02-29 07:34:17 +0500

    Hide past events

--- apps/builddao/widget/events/ListView.jsx
+++ apps/builddao/widget/events/ListView.jsx
@@ -91,117 +91,163 @@ const formatStartTime = (time) => {
 
 const dateKeys = Object.keys(categorizedEvents);
 
-return (
-  <EventsContainer>
-    {!dateKeys.length && <p className="text-white">No events this month</p>}
-    {dateKeys.map((date, i) => (
-      <div className="d-flex gap-5">
-        <h3 className="flex-shrink-0 text-white" style={{ minWidth: 65 }}>
-          <div className="d-flex gap-3 align-items-center">
-            {date.split(" ").map((it, i) => (
-              <span
-                style={{
-                  fontSize: i === 0 ? "24px" : "16px",
-                  color:
-                    i === 0
-                      ? "var(--text-color, #fff)"
-                      : "var(--white-50, #CDD0D5)",
-                }}
-              >
-                {it}
-              </span>
-            ))}
-          </div>
-        </h3>
-        <div className="w-100 d-flex flex-column gap-3">
-          {categorizedEvents[date].map((event, i) => {
-            const hashtags =
-              event?.extendedProps?.hashtags.map((it) => {
-                if (it.customOption) {
-                  return it.hashtags;
-                }
-                return it;
-              }) ?? [];
-            const organizers =
-              event?.extendedProps?.organizers.map((it) => {
-                if (it.customOption) {
-                  return it.organizer;
-                }
-                return it;
-              }) ?? [];
+const today = new Date().getDate();
+const pastEvents = dateKeys.filter((date) => {
+  const day = date.split(" ")[0];
+  return parseInt(day) < today;
+});
+const futureEvents = dateKeys.filter((date) => {
+  const day = date.split(" ")[0];
+  return parseInt(day) >= today;
+});
 
-            const organizer = organizers[0];
-            const organizerProfile = Social.getr(`${organizer}/profile`);
+const EventGroup = ({ date }) => {
+  const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 32px;
 
-            const startTime = formatStartTime(event.start);
+    @media screen and (max-width: 768px) {
+      flex-direction: column;
+      gap: 24px;
+    }
+  `;
 
-            return (
-              <StyledEvent key={`event-${i}`}>
-                <div className="d-flex align-items-center justify-content-between w-100">
-                  <div className="d-flex align-items-center gap-2 flex-wrap">
-                    {hashtags.map((tag) => (
-                      <Hashtag key={tag}>{tag}</Hashtag>
-                    ))}
-                  </div>
-                  <span>{startTime}</span>
+  return (
+    <Container>
+      <h3 className="flex-shrink-0 text-white" style={{ minWidth: 65 }}>
+        <div className="d-flex gap-3 align-items-center">
+          {date.split(" ").map((it, i) => (
+            <span
+              style={{
+                fontSize: i === 0 ? "24px" : "16px",
+                color:
+                  i === 0
+                    ? "var(--text-color, #fff)"
+                    : "var(--white-50, #CDD0D5)",
+              }}
+            >
+              {it}
+            </span>
+          ))}
+        </div>
+      </h3>
+      <div className="w-100 d-flex flex-column gap-3">
+        {categorizedEvents[date].map((event, i) => {
+          const hashtags =
+            event?.extendedProps?.hashtags.map((it) => {
+              if (it.customOption) {
+                return it.hashtags;
+              }
+              return it;
+            }) ?? [];
+          const organizers =
+            event?.extendedProps?.organizers.map((it) => {
+              if (it.customOption) {
+                return it.organizer;
+              }
+              return it;
+            }) ?? [];
+
+          const organizer = organizers[0];
+          const organizerProfile = Social.getr(`${organizer}/profile`);
+
+          const startTime = formatStartTime(event.start);
+
+          return (
+            <StyledEvent key={`event-${i}`}>
+              <div className="d-flex align-items-center justify-content-between w-100">
+                <div className="d-flex align-items-center gap-2 flex-wrap">
+                  {hashtags.map((tag) => (
+                    <Hashtag key={tag}>{tag}</Hashtag>
+                  ))}
                 </div>
-                <div className="d-flex gap-3">
-                  <div className="cover-image">
-                    <Widget
-                      src="mob.near/widget/Image"
-                      props={{
-                        image: event.extendedProps.cover,
-                        fallbackUrl:
-                          "https://ipfs.near.social/ipfs/bafkreibas66y6ewop5ix2n6mgybpjz6epg7opqvcplmm5jw4jlhdik5nhe",
-                      }}
-                    />
-                  </div>
-                  <div>
-                    <h4>{event.title}</h4>
-                    <Markdown text={event.description} />
-                  </div>
-                </div>
-                <div className="d-flex align-items-center flex-wrap gap-3">
-                  <span className="d-flex align-items-center gap-1">
-                    <Widget
-                      src="mob.near/widget/Image"
-                      loading=""
-                      props={{
-                        image: organizerProfile.image,
-                        fallbackUrl:
-                          "https://ipfs.near.social/ipfs/bafkreibas66y6ewop5ix2n6mgybpjz6epg7opqvcplmm5jw4jlhdik5nhe",
-                        style: {
-                          width: 24,
-                          height: 24,
-                          borderRadius: 12,
-                          objectFit: "cover",
-                        },
-                      }}
-                    />
-                    {organizerProfile.name ??
-                      organizers[0] ??
-                      "No name profile"}
-                  </span>
-                  <span className="d-flex align-items-center gap-1">
-                    <i className="bi bi-geo-alt"></i>
-                    {event?.extendedProps?.location}
-                  </span>
+                <span>{startTime}</span>
+              </div>
+              <div className="d-flex gap-3">
+                <div className="cover-image">
+                  <Widget
+                    src="mob.near/widget/Image"
+                    props={{
+                      image: event.extendedProps.cover,
+                      fallbackUrl:
+                        "https://ipfs.near.social/ipfs/bafkreibas66y6ewop5ix2n6mgybpjz6epg7opqvcplmm5jw4jlhdik5nhe",
+                    }}
+                  />
                 </div>
                 <div>
-                  <Button
-                    noLink={true}
-                    href={`${event?.url}`}
-                    target="_blank"
-                    variant="primary"
-                  >
-                    Join Now
-                  </Button>
+                  <h4>{event.title}</h4>
+                  <Markdown text={event.description} />
                 </div>
-              </StyledEvent>
-            );
-          })}
-        </div>
+              </div>
+              <div className="d-flex align-items-center flex-wrap gap-3">
+                <span className="d-flex align-items-center gap-1">
+                  <Widget
+                    src="mob.near/widget/Image"
+                    loading=""
+                    props={{
+                      image: organizerProfile.image,
+                      fallbackUrl:
+                        "https://ipfs.near.social/ipfs/bafkreibas66y6ewop5ix2n6mgybpjz6epg7opqvcplmm5jw4jlhdik5nhe",
+                      style: {
+                        width: 24,
+                        height: 24,
+                        borderRadius: 12,
+                        objectFit: "cover",
+                      },
+                    }}
+                  />
+                  {organizerProfile.name ?? organizers[0] ?? "No name profile"}
+                </span>
+                <span className="d-flex align-items-center gap-1">
+                  <i className="bi bi-geo-alt"></i>
+                  {event?.extendedProps?.location}
+                </span>
+              </div>
+              <div>
+                <Button
+                  noLink={true}
+                  href={`${event?.url}`}
+                  target="_blank"
+                  variant="primary"
+                >
+                  Join Now
+                </Button>
+              </div>
+            </StyledEvent>
+          );
+        })}
       </div>
+    </Container>
+  );
+};
+
+const PastEvents = () => {
+  return (
+    <>
+      {!pastEvents.length && (
+        <p className="text-white">No past events this month</p>
+      )}
+      {pastEvents.map((date, i) => (
+        <EventGroup date={date} />
+      ))}
+    </>
+  );
+};
+
+const [showPastEvents, setShowPastEvents] = useState(false);
+
+return (
+  <EventsContainer>
+    <Button onClick={() => setShowPastEvents((prev) => !prev)}>
+      {showPastEvents ? "Hide" : "Show"} Past Events
+    </Button>
+    {showPastEvents && <PastEvents />}
+    {!futureEvents.length && (
+      <p className="text-white">No upcoming events this month</p>
+    )}
+    {futureEvents.map((date, i) => (
+      <EventGroup date={date} />
     ))}
   </EventsContainer>
 );
