38fda4f71cb8e3d1dd0ca86a520db76a8b0d56f3
Muhammad Zeeshan Ahmad <itexpert120@outlook.com>
2024-03-04 10:41:31 +0500

    Fix github link in footer

--- apps/builddao/widget/home/Footer.jsx
+++ apps/builddao/widget/home/Footer.jsx
@@ -127,7 +127,7 @@ const Footer = ({ noBanner }) => {
           <a href="https://nearbuilders.com/tg-builders" target="_blank">
             <i className="bi bi-telegram"></i>
           </a>
-          <a href="https://github.near/NEARBuilders" target="_blank">
+          <a href="https://github.com/NEARBuilders" target="_blank">
             <i className="bi bi-github"></i>
           </a>
           <a href="https://devs.near.social/" target="_blank">
