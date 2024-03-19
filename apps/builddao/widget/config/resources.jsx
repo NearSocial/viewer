a6c8addb81ee8052a5bb196b9ce8d789b96c1da9
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-03-19 09:59:17 -0400

    remove library from resources

--- apps/builddao/widget/config/resources.jsx
+++ apps/builddao/widget/config/resources.jsx
@@ -11,15 +11,5 @@ return {
           "https://raw.githubusercontent.com/NEARBuilders/gateway/main/resources.md",
       },
     },
-    library: {
-      path: "buildhub.near/widget/Resources",
-      blockHeight: "final",
-      init: {
-        name: "Library",
-        icon: "bi-collection",
-        mdPath:
-          "https://raw.githubusercontent.com/NEARBuilders/docs/main/build-library.md",
-      },
-    },
   },
 };
